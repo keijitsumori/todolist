f76ffa1542ef569b51133ce4ce6c1437e700cb5c
keijitsumori <kjmikann3035@docomo.ne.jp>
2021-09-21 05:04:58 +0000

    [finish]

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '93e122ca4fde9fe596c6db51b9d37ed7213becc51855724ece68980912ad24032a83e9ee97e1c673128d8593b8dd27f1448de3034c9c4a36e9402bff0286a0cc'
+
